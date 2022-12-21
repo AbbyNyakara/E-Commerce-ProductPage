c6df85689b4b019f3e6035e7319ef69a78098b34
Abby Nyakara <abbynyakara@gmail.com>
2022-12-21 20:53:32 +0300

    toggle images

--- src/components/PhotoDisplay/PhotoDisplay.tsx
+++ src/components/PhotoDisplay/PhotoDisplay.tsx
@@ -8,27 +8,18 @@ import thumbnail2 from '/images/image-product-2-thumbnail.jpg';
 import thumbnail3 from '/images/image-product-3-thumbnail.jpg';
 import thumbnail4 from '/images/image-product-4-thumbnail.jpg';
 
-// @ts-ignore
-import Data from './photos.js';
-
-
 const PhotoDisplay = () => {
   return (
     <div className='photo-display'>
-      {
-        Data.map((photo: any, index: number) => {
-          return(
-            <div className="img__display" key={index}>
-              <div className="photo-display__main-img">
-                <img src={photo.img} alt="" />
-              </div>
-              <div className="photo-display__small-img">
-                <img src={photo.thumbnail} className='photo-display__thumbnail-img' alt="image-thumbnail" />
-              </div>  
-            </div>
-          )
-        })
-      }
+      <div className="photo-display__main-img">
+        <img src={img1} alt="" />
+      </div>
+      <div className="photo-display__small-img">
+        <img src={thumbnail1} className='photo-display__thumbnail-img active' alt="image-thumbnail" />
+        <img src={thumbnail2} className='photo-display__thumbnail-img' alt="image-thumbnail" />
+        <img src={thumbnail3} className='photo-display__thumbnail-img' alt="image-thumbnail" />
+        <img src={thumbnail4} className='photo-display__thumbnail-img' alt="image-thumbnail" />
+      </div>
     </div>
   )
 }
